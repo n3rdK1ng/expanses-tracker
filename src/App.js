286cf836588cee8ec1a1e198080ab2f60e02711f
Marco Velan <marco.velan10@gmail.com>
2022-12-26 11:54:39 +0100

    add react state and work with events

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
@@ -23,12 +26,17 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
